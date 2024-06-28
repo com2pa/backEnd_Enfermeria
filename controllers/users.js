8a07bf12271cc79998b7c95127e3e04efac3eb9f
merwil vegas <com2pa@gmail.com>
2024-06-28 14:19:05 -0400

    se completo la notificacion despues de registar y envio de mensaje al correo para su verificacion

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,11 @@ const { request, response } = require("../app");
 const usersRouter = require('express').Router();
 const bcrypt = require('bcrypt')
 const User = require('../models/user')
+// envio de confirmacion de correo
+const jwt = require('jsonwebtoken')
+const nodemailer = require("nodemailer");
+const { PAGE_URL } = require('../config');
+
 
 usersRouter.post('/', async(request,response)=>{
    const {name,
@@ -13,23 +18,55 @@ usersRouter.post('/', async(request,response)=>{
     if(!name || !email || !phone || !password ){
         return response.status(400).json({error:'Todos los campon son requerido'})
     }
+    const userExist = await User.findOne({email})
+    if(userExist){
+        return response.status(400).json({error:'El email ya se encuentra en uso !'})
+    }
     // return response.sendStatus(200);
 
-    // encriptando la constraseña
+//     // encriptando la constraseña
     const saltRounds = 10;
     const passwordHash= await bcrypt.hash(password,saltRounds)
-    console.log(passwordHash);
+    // console.log(passwordHash);
     // creando nuevo usuario en la base de datos
     const newUser = new User({
         name,email,phone,password:passwordHash
     })
-    // guardando usuario registrado 
+//     // guardando usuario registrado 
     const savedUser = await newUser.save();
-    console.log(savedUser);
-
+    // console.log(savedUser);
+//     // el token dura 1 un dia!
+    const token = jwt.sign({id: savedUser.id}, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1d'
+    });
+    // console.log(token);
     // enviar correo para verificacion de usuaruio registrado
 
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, // Use `true` for port 465, `false` for all other ports
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        },
+    });
 
+//     //  como enviar el correo
+    // async function main() {
+        // send mail with defined transport object
+        await transporter.sendMail({
+          from: process.env.EMAIL_USER, // sender address
+          to: email, // list of receivers
+          subject: "Verificacion de usuario :)  ✔", // Subject line
+          text: "Acontinuacion se presenta este link para poder validar tu registro en la pagina", 
+          html: `<a href="${PAGE_URL}/verify${token}">Verificar Correo por favor ! </a>`, 
+        });
+      
+
+//     // }
+    // console.log(transporter);
+    return response.status(201).json('¡ USUARIO CREADO! :)   , verifique su correo para confirmar !   ');
 
 });
 
