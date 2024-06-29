09d9bdd4aa982f1ae187674566c408d9e92f8c1a
merwil vegas <com2pa@gmail.com>
2024-06-29 17:08:22 -0400

    se añadio una funcionar para pasar por parametro id y token

--- controllers/users.js
+++ controllers/users.js
@@ -34,14 +34,16 @@ usersRouter.post('/', async(request,response)=>{
     })
 //     // guardando usuario registrado 
     const savedUser = await newUser.save();
-    // console.log(savedUser);
-//     // el token dura 1 un dia!
+    console.log(savedUser);
+
+    // el token dura 1 un dia!
     const token = jwt.sign({id: savedUser.id}, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: '1d'
     });
     // console.log(token);
-    // enviar correo para verificacion de usuaruio registrado
 
+
+    // enviar correo para verificacion de usuaruio registrado
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 465,
@@ -60,14 +62,71 @@ usersRouter.post('/', async(request,response)=>{
           to: email, // list of receivers
           subject: "Verificacion de usuario :)  ✔", // Subject line
           text: "Acontinuacion se presenta este link para poder validar tu registro en la pagina", 
-          html: `<a href="${PAGE_URL}/verify${token}">Verificar Correo por favor ! </a>`, 
+          html: `<a href="${PAGE_URL}/verify/${savedUser.id}/${token}">Verificar Correo por favor ! </a>`, 
         });
       
 
 //     // }
     // console.log(transporter);
-    return response.status(201).json('¡ USUARIO CREADO! :)   , verifique su correo para confirmar !   ');
+    return response.status(201).json('¡ USUARIO CREADO! :)   , verifique su correo para confirmar !   ',);
 
 });
 
+
+usersRouter.patch('/:id/:token', async(request,response)=>{
+    try {
+        const token= request.params.token;
+        
+        
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        const id = decodedToken.id;
+
+        // cambiando la propiedad de la base de datos verificacion de correo a true 
+        await User.findByIdAndUpdate(id, {verificacion: true})
+        return response.sendStatus(200)
+
+    } catch (error) {
+    //    encontrar el email del usuario
+        const id = request.params.id;
+        const {email} = await User.findById(id);
+    //    console.log(email)
+
+
+         // firmar el nuevo token
+        const token = jwt.sign({id: id}, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: '1d'
+        });
+    
+     // enviar correo para verificacion de usuaruio registrado
+    
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true, // Use `true` for port 465, `false` for all other ports
+            auth: {
+              user: process.env.EMAIL_USER,
+              pass: process.env.EMAIL_PASS,
+            },
+        });
+    
+            //  como enviar el correo
+   
+            await transporter.sendMail({
+              from: process.env.EMAIL_USER, // sender address
+              to: email, // list of receivers
+              subject: "Verificacion de usuario :)  ✔", // Subject line
+              text: "Acontinuacion se presenta este link para poder validar tu registro en la pagina", 
+              html: `<a href="${PAGE_URL}/verify/${id}/${token}">Verificar Correo por favor ! </a>`, 
+            });
+
+
+
+
+
+            return response.status(400).json({error:'El link expiro. Se ha enviado un ¡Nuevo link! de verificacion a su correo'})
+    }
+
+ 
+
+});
 module.exports = usersRouter;
