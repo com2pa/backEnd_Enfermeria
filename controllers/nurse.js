888ddff10bf99d6776f3efb0a3611c2cc864d394
merwil vegas <com2pa@gmail.com>
2024-07-30 22:13:13 -0400

    se añadio para ver todos los servicio no completo en mejoras

--- controllers/nurse.js
+++ controllers/nurse.js
@@ -1,6 +1,7 @@
 const Nurse = require('../models/nurse');
 const mongoose = require('mongoose')
 const nurseRouter = require('express').Router();
+const Service = require('../models/service')
 
 nurseRouter.get('/', async (request, response) => {
     // nos muestra el usuario logiado 
@@ -21,14 +22,18 @@ nurseRouter.post('/', async(request,response)=>{
         return response.status(403).json('No estas autorizado para esta funcion');
     }
     // lo que envia el usuario en el front end
-    const {name} = request.body
+    const {name,servicioId} = request.body
     console.log('se envia enfermero : ',name)
+    console.log('se envia enfermero : ',servicioId)
+
+    
 
 
     // creo un nuevo enfermero
     const newNurse = new Nurse({
         name,
-        user: user._id,
+        user: user.id,
+        
     })
     // guardando enfermero registrado
     const savedNurse = await newNurse.save();
@@ -47,13 +52,20 @@ nurseRouter.put('/:id', async(request, response)=>{
     }
     // lo que envia el usuario en el front end
     const {name} = request.body
+    // const {idSelect}= request.params.id
     console.log('se envia enfermero : ',name)
+    // console.log('id de servicio',idSelect)
 
     // busco el enfermero
     const nurse = await Nurse.findById(request.params.id);
     if (!nurse) {
         return response.status(404).json('No se encontro el enfermero');
     }
+    // actualizando el enfermero
+    // agregando el nuevo servicio
+    // nurse.servicios.push(idSelect);
+
+
     //
     nurse.name = name;
 
@@ -91,6 +103,41 @@ nurseRouter.delete('/:id', async(request, response)=>{
 
 
     return response.status(200).json('enfermero eliminado')
+});
+
+
+
+
+
+// 
+nurseRouter.put('/:id', async(request, response) => {
+    // busco al usuario
+    const user = request.user;
+    if (user.role !== 'admin') {
+        return response.status(403).json('No estas autorizado para esta funcion');
+    }
+
+    // lo que envia el  frontend
+    const{ servicioId} = request.body;
+    const { id } = request.body;
+    console.log(id)
+    console.log(servicioId)
+
+    // // busco al enfermero por el id
+    // const nurse = await Nurse.findById(id);
+    if (!Nurse) {
+        return response.status(404).json('No se encontro el enfermero');
+    }
+    // // verifico si el servioId esta en el servicio del enfermero si no lo añado
+    const serviceExists = Nurse.service.some(service => service.service.toString() === servicioId);
+    if (!serviceExists) {
+        Nurse.service.push({service:servicioId}); // añado el servicioId al array de servicios
+        console.log('se agrego el servicio ', Nurse.service)
+        // lo guardo en la base de datos
+        await Nurse.save();
+        return response.status(201).json('servicio añadido al enfermero');
+    }
+   
 });
 
 module.exports = nurseRouter;
