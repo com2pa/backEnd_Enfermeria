7c4beea3b7fab77b7f1383ecdc59ef0b750db64a
merwil vegas <com2pa@gmail.com>
2024-07-30 22:15:35 -0400

    se borro un atributo del modelo

--- models/nurse.js
+++ models/nurse.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
-const { Transform } = require('nodemailer/lib/xoauth2');
+
 
 // document
 const nurseSchema = new mongoose.Schema({
     name: String,
+    service:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Service'
+    }
         
 })
 
-nurseSchema.set('',{
+nurseSchema.set('toJSON',{
     Transform:(document, returnedObject)=>{
         returnedObject.id = returnedObject._id.toString();
         delete returnedObject._id;
